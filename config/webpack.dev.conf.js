e64f5bdaf84b94483f30a3d0c4a3c202e59eea4a
TonySergio <slayerid.montano@gmail.com>
2018-08-27 01:20:16 +0300

    electron started success

---
+++ config/webpack.dev.conf.js
@@ -0,0 +1,49 @@
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+var paths = require('./paths');
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = [
+    require.resolve('react-hot-loader/patch'),
+    require.resolve('webpack-dev-server/client') + '?/',
+    require.resolve('webpack/hot/only-dev-server')
+    //'./polyfills'
+  ].concat(baseWebpackConfig.entry[name])
+})
+
+module.exports = merge(baseWebpackConfig, {
+  output: {
+    // Next line is not used in dev but WebpackDevServer crashes without it:
+    path: paths.appBuild,
+    pathinfo: true,
+    filename: 'static/js/bundle.js',
+    publicPath: '/'
+  },
+
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml,
+      favicon: paths.appFavicon,
+    }),
+    new FriendlyErrorsPlugin()
+  ]
+})
